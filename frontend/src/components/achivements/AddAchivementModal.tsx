f492d90b3da133d42fb2a03a8b9c5a6592c17e40
nirav.k <niravkothiya2@gmail.com>
2024-10-24 01:50:18 +0530

    fix: upload ui upgraded

--- frontend/src/components/achivements/AddAchivementModal.tsx
+++ frontend/src/components/achivements/AddAchivementModal.tsx
@@ -42,45 +42,81 @@ const AddAchivementModal = ({ onClose, onUpload }: any) => {
             Add Achievement
           </h3>
 
-          {/* File Input */}
-          <div className="mb-4">
-            <input type="file" name="file" onChange={handleFileChange} />
-          </div>
-
           {/* Name Input */}
           <div className="mb-4">
-            <label>Name:</label>
             <input
               type="text"
               value={name}
+              placeholder="Name"
               onChange={(e) => setName(e.target.value)}
               required
               className="w-full border border-gray-300 p-2 rounded"
             />
           </div>
 
-          {/* X Input */}
-          <div className="mb-4">
-            <label>X:</label>
-            <input
-              type="number"
-              value={x}
-              onChange={(e) => setX(Number(e.target.value))}
-              required
-              className="w-full border border-gray-300 p-2 rounded"
-            />
+          <div className="flex gap-4 mb-4">
+            {/* X Input */}
+            <div className="w-1/2">
+              <input
+                type="number"
+                value={x}
+                placeholder="X"
+                onChange={(e) => setX(Number(e.target.value))}
+                required
+                className="w-full border border-gray-300 p-2 rounded"
+              />
+            </div>
+
+            {/* Y Input */}
+            <div className="w-1/2">
+              <input
+                type="number"
+                value={y}
+                placeholder="Y"
+                onChange={(e) => setY(Number(e.target.value))}
+                required
+                className="w-full border border-gray-300 p-2 rounded"
+              />
+            </div>
           </div>
 
-          {/* Y Input */}
-          <div className="mb-4">
-            <label>Y:</label>
-            <input
-              type="number"
-              value={y}
-              onChange={(e) => setY(Number(e.target.value))}
-              required
-              className="w-full border border-gray-300 p-2 rounded"
-            />
+          {/* File Input */} 
+          <div
+            className="flex items-center justify-center w-full mb-4"
+            onChange={handleFileChange}
+          >
+            <label className="flex flex-col items-center justify-center w-full h-[10rem] border-2 border-gray-300 border-dashed rounded-lg cursor-pointer hover:bg-gray-100">
+              <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                <svg
+                  className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 16"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                  />
+                </svg>
+                <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                  <span className="font-semibold">Click to upload</span>
+                </p>
+                <p className="text-xs text-gray-500 dark:text-gray-400">
+                  SVG, PNG, JPG
+                </p>
+              </div>
+              <input
+                id="dropzone-file"
+                type="file"
+                accept="image/*"
+                className="hidden"
+                required
+              />
+            </label>
           </div>
 
           {/* Image Preview */}
